d14ea4e65317dbcc3c48bfb2f7cf90cbda94afb5
Filimonsha <Filimonshacs@mail.ru>
2022-04-05 19:11:41 +0300

    ref input on registration and login page

--- src/views/auth/Registration.jsx
+++ src/views/auth/Registration.jsx
@@ -18,7 +18,6 @@ function Registration() {
     }
     return (
         <Container>
-            
             <Row className="justify-content-center">
                 <Col lg={'4'}>
                     {/* <div className="container registration"> */}
@@ -28,55 +27,66 @@ function Registration() {
                             <input
                                 type="text"
                                 placeholder="Почта или телефон"
-                                className="registration__input registration__password-wrap__password"
+                                className="registration__input registration__email registration__password-wrap__password"
                             />
                         </Form.Group>
-                        <Form.Group controlId="formBasicEmail">
-                            <div className="registration__password-wrap">
+                        <Form.Group>
+                            <div class="input-group flex-nowrap">
                                 <input
                                     ref={password}
                                     type="password"
                                     placeholder="Пароль"
-                                    className="registration__input registration__password-wrap__password"
+                                    className="login__input login__password-wrap__password"
                                 />
-                                <svg
-                                    className="registration__password-wrap__svg"
-                                    onClick={passwordButtonHandler}
-                                    width="22"
-                                    height="10"
-                                    viewBox="0 0 22 10"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M19 1C19 1 18.6797 1.66735 18 2.51445M11 6C9.39201 6 8.04786 5.58775 6.94861 5M11 6C12.608 6 13.9521 5.58775 15.0514 5M11 6V9.5M3 1C3 1 3.35367 1.73682 4.10628 2.64476M6.94861 5L4 8M6.94861 5C5.6892 4.32661 4.75124 3.42285 4.10628 2.64476M15.0514 5L17.5 8M15.0514 5C16.3818 4.28865 17.3535 3.32023 18 2.51445M4.10628 2.64476L1 4M18 2.51445L21 4"
-                                        stroke="black"
-                                        stroke-linecap="round"
-                                    />
-                                </svg>
+
+                                <div class="input-group-prepend">
+                                    <svg
+                                        className="login__password-wrap__svg "
+                                        id="addon-wrapping"
+                                        onClick={passwordButtonHandler}
+                                        width="22"
+                                        height="10"
+                                        viewBox="0 0 22 10"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M19 1C19 1 18.6797 1.66735 18 2.51445M11 6C9.39201 6 8.04786 5.58775 6.94861 5M11 6C12.608 6 13.9521 5.58775 15.0514 5M11 6V9.5M3 1C3 1 3.35367 1.73682 4.10628 2.64476M6.94861 5L4 8M6.94861 5C5.6892 4.32661 4.75124 3.42285 4.10628 2.64476M15.0514 5L17.5 8M15.0514 5C16.3818 4.28865 17.3535 3.32023 18 2.51445M4.10628 2.64476L1 4M18 2.51445L21 4"
+                                            stroke="black"
+                                            stroke-linecap="round"
+                                        />
+                                    </svg>
+
+                                </div>
                             </div>
                         </Form.Group>
-                        <Form.Group controlId="formBasicEmail">
-                            <div className="registration__password-wrap">
+
+                        <Form.Group>
+                            <div class="input-group flex-nowrap">
                                 <input
                                     ref={repeatPassword}
                                     type="password"
                                     placeholder="Повторите пароль"
                                     className="registration__input registration__password-wrap__password"
                                 />
-                                <svg
-                                    className="registration__password-wrap__svg"
-                                    onClick={passwordButtonHandler}
-                                    width="22"
-                                    height="10"
-                                    viewBox="0 0 22 10"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M19 1C19 1 18.6797 1.66735 18 2.51445M11 6C9.39201 6 8.04786 5.58775 6.94861 5M11 6C12.608 6 13.9521 5.58775 15.0514 5M11 6V9.5M3 1C3 1 3.35367 1.73682 4.10628 2.64476M6.94861 5L4 8M6.94861 5C5.6892 4.32661 4.75124 3.42285 4.10628 2.64476M15.0514 5L17.5 8M15.0514 5C16.3818 4.28865 17.3535 3.32023 18 2.51445M4.10628 2.64476L1 4M18 2.51445L21 4"
-                                        stroke="black"
-                                        stroke-linecap="round"
-                                    />
-                                </svg>
+
+                                <div class="input-group-prepend">
+                                    <svg
+                                        className="login__password-wrap__svg "
+                                        id="addon-wrapping"
+                                        onClick={passwordButtonHandler}
+                                        width="22"
+                                        height="10"
+                                        viewBox="0 0 22 10"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M19 1C19 1 18.6797 1.66735 18 2.51445M11 6C9.39201 6 8.04786 5.58775 6.94861 5M11 6C12.608 6 13.9521 5.58775 15.0514 5M11 6V9.5M3 1C3 1 3.35367 1.73682 4.10628 2.64476M6.94861 5L4 8M6.94861 5C5.6892 4.32661 4.75124 3.42285 4.10628 2.64476M15.0514 5L17.5 8M15.0514 5C16.3818 4.28865 17.3535 3.32023 18 2.51445M4.10628 2.64476L1 4M18 2.51445L21 4"
+                                            stroke="black"
+                                            stroke-linecap="round"
+                                        />
+                                    </svg>
+
+                                </div>
                             </div>
                         </Form.Group>
                         <div className="registration__submit">
@@ -86,7 +96,6 @@ function Registration() {
                             </Link>
                         </div>
                     </Form>
-                    {/* </div> */}
                 </Col>
             </Row>
         </Container>
