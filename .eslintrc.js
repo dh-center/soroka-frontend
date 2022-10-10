32d9decdc3357392b6d0b8fd96f1182c8332be83
AlxRbkv <wsro2012@mail.ru>
2022-10-10 14:21:21 +0400

    SOROKA-35: added eslinter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ['prettier'],
+    extends: ['react-app', 'eslint:recommended', 'plugin:react/recommended', 'airbnb-base', 'prettier'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
@@ -17,7 +17,10 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         // allow jsx syntax in js files (for next.js project)
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'react-hooks/rules-of-hooks': 'error'
-        // 'react-hooks/exhaustive-deps': 'warn'
+        'no-unused-vars': 'warn',
+        'unused-imports/no-unused-imports': 'warn',
+        'no-const-assign': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
     }
 }
