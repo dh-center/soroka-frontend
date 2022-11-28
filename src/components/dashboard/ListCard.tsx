ec348b132c23f5196b5960b3596ca9f47f5ddf6a
AlxRbkv <wsro2012@mail.ru>
2022-11-28 21:17:43 +0400

    SOROKA-67: cover added (replace url path)

--- src/components/dashboard/ListCard.tsx
+++ src/components/dashboard/ListCard.tsx
@@ -1,10 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Badge, Card, Container } from 'react-bootstrap'
 import { CardImage } from 'react-bootstrap-icons'
 import { FormattedMessage } from 'react-intl'
 import { useNavigate } from 'react-router-dom'
 import { getCardById } from 'utils/urls'
-import CardsAPI from 'api/cards'
 
 type ListCardProps = {
     id: number
@@ -15,38 +14,43 @@ type ListCardProps = {
 
 const ListCard = ({ id, name, isFilled = true, cover = null }: ListCardProps) => {
     const navigate = useNavigate()
+    const [cardCover, setCardCover] = useState<string>('')
 
-    console.log(cover, 'cover')
     useEffect(() => {
         try {
             if (cover) {
                 const fileJSON = JSON.parse(cover)
-                console.log(fileJSON, 'js')
-                CardsAPI.uploadFileById(fileJSON.id, fileJSON.name).then((res) => console.log(res, 'res'))
+                setCardCover(
+                    fileJSON.url.replace(
+                        'https://localhost/restapi/v1/',
+                        'https://soroka.f128.science/restapi/v1/files/by-id/'
+                    )
+                )
             }
         } catch (e) {
-            console.warn('Не удалось преоьразовать строку в JSON')
+            console.warn('Не удалось преобразовать строку в JSON')
         }
-    }, [])
+    }, [cover])
 
     return (
         <Card onClick={() => navigate(getCardById(id))} role="button">
-            <Card.Img as={Container} variant="top" className="position-relative">
-                <div style={{ height: '150px', backgroundColor: 'white' }}></div>
-                {cover === null ? (
+            {cardCover ? (
+                <Card.Img variant="top" className="position-relative" src={cardCover} />
+            ) : (
+                <Card.Img as={Container} variant="top" className="position-relative">
+                    <div style={{ height: '150px', backgroundColor: 'white' }}></div>
                     <CardImage size={48} color="black" className="position-absolute top-50 start-50 translate-middle" />
-                ) : (
-                    <img src="" alt="card cover" />
-                )}
 
-                {!isFilled && (
-                    <Container className="position-absolute bottom-0 start-0 p-2 d-flex justify-content-end">
-                        <Badge bg="warning" text="dark">
-                            <FormattedMessage id="notFilled" />
-                        </Badge>
-                    </Container>
-                )}
-            </Card.Img>
+                    {!isFilled && (
+                        <Container className="position-absolute bottom-0 start-0 p-2 d-flex justify-content-end">
+                            <Badge bg="warning" text="dark">
+                                <FormattedMessage id="notFilled" />
+                            </Badge>
+                        </Container>
+                    )}
+                </Card.Img>
+            )}
+
             <Card.Footer>{name}</Card.Footer>
         </Card>
     )
